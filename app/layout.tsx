d35a98b495f93b96b5e10528cbd4417ff39463fd
Chris Pham <chris@Chriss-MacBook-Pro.local>
2026-03-15 12:23:16 -0700

    forgot export

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,3 +30,15 @@ export const metadata: Metadata = {
     images: ["/og-image.png"],
   },
 };
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
